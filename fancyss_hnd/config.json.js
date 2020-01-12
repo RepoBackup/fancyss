6abd5013e2b52f214dfb5bf9853808fb96a72427
Zed <zed@zedd.dev>
2020-01-12 17:50:19 +0800

    hnd: bump ss-* to 3.3.4;

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-12-10_01:17:16",
+"build_date":"2020-01-12_17:48:49",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"0bf374145ae10cd8d95ad8d446d95e75",
+"md5":"5cc1707c61269e70fb4aa7c063e05033",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.8.1"
+"version":"1.8.3"
 }
